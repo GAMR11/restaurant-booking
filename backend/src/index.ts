aa83abd9f57c2869e12b668f949d090f6b3b192a
gamr11 <gamr1308988@gmail.com>
2025-11-16 21:22:41 -0500

    add cors in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'front-restaurant-booking.railway.internal', // Agrega tu dominio de Railway
+    /\.up\.railway\.app$/  // Permite cualquier subdominio de Railway
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
